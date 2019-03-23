d66e142b3f538b6b5c8ea4161fcc9bdfdaefbb2d
AlFalah <taiebalfalah@gmail.com>
2019-03-23 23:12:21 +0100

    Cpp Baby step

--- Baby_Stepwithcpp/main.cpp
+++ Baby_Stepwithcpp/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    std::cout << "Yoo Chdoula" << std::endl;
+    std::
+    // cout << "Yoo Chdoula" << std::endl;
+     cout << "this is a string Chdoulaaaaaaaaa\n" 
+        << "this is another stirng Chdoula\n" 
+        << "and a final string ya chdoulaaaaaaaaa\n"
+       << std::endl; 
     std::cin.get();
 }
