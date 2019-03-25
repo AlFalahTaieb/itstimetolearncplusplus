199d9307854b6f3b85e8fbf2bc878a884bc5be2a
AlFalah <taiebalfalah@gmail.com>
2019-03-25 22:35:56 +0100

    🍼

--- baby_Step/main.cpp
+++ baby_Step/main.cpp
@@ -2,13 +2,22 @@
 
 int sayHello() //Defining function
 {
-    std::cout //console output
+    std::cout                                   //console output
         << "this is a string Chdoulaaaaaaaaa\n" //<< allow us to send letters or numbers to be outputed
         << "this is another stirng Chdoula\n"
         << "and a final string ya chdoulaaaaaaaaa\n";
 }
 
+int variableAssign()
+{
+    int width(5);
+
+    width = 7;
+    std::cout << width;
+}
+
 int main()
 {
-    sayHello();
+    // sayHello();
+    variableAssign();
 }
