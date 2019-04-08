759940ca11e2ae77de26b4ca2d16560f5f8e5316
AlFalah <taiebalfalah@gmail.com>
2019-04-08 16:29:04 +0200

    🍼

---
+++ baby_Step/functions/artihmatic.h
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+// int add(int x, int y, int z)
+// {
+//     // return x + y + z;
+//     int res = x + y + z;
+//     std::cout << "this is SPART";
+// }
+
+int multiply(int x, int y)
+{
+   int result = x * y;
+    return result;
+}
